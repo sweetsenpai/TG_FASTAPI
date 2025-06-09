d59cce6314b0b2413bd93d13cce9555e3234251c
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-06-09 15:18:04 +0300

    Полностью рабочий прототип проекта решающий поставленные задачи

--- bot/main.py
+++ bot/main.py
@@ -17,78 +17,34 @@ bot.
 
 import logging
 import os
+import re
 
-# import httpx
+import httpx
 from dotenv import load_dotenv
+from logger_config import setup_logger
 from telegram import ForceReply, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Application,
     CallbackQueryHandler,
     CommandHandler,
     ContextTypes,
-    MessageHandler,
-    filters,
 )
 
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
+
+
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-logger = logging.getLogger(__name__)
-
-test_response = [
-    {
-        "title": "string",
-        "text": "string",
-        "id": 4,
-        "date_of_creation": "2025-06-08T15:18:00.438230Z",
-    },
-    {
-        "title": "Юбилейный тестовый пост",
-        "text": "Это юбилейный тестовый текст",
-        "id": 5,
-        "date_of_creation": "2025-06-08T15:55:00.979759Z",
-    },
-    {
-        "title": "Поменяли",
-        "text": "Лалалалалалалал",
-        "id": 3,
-        "date_of_creation": "2025-06-08T15:16:50.817298Z",
-    },
-    {
-        "title": "string",
-        "text": "",
-        "id": 6,
-        "date_of_creation": "2025-06-08T16:19:51.313937Z",
-    },
-    {
-        "title": "",
-        "text": "",
-        "id": 7,
-        "date_of_creation": "2025-06-08T16:19:57.626386Z",
-    },
-    {
-        "title": "Мой первый пост",
-        "text": "qwdfqwdqwe",
-        "id": 8,
-        "date_of_creation": "2025-06-08T16:37:41.762078Z",
-    },
-    {
-        "title": "Мой не первый пост",
-        "text": "Это содержимое моего не первого поста поста",
-        "id": 9,
-        "date_of_creation": "2025-06-09T09:25:19.725538Z",
-    },
-]
+logger = setup_logger(__name__)
+logger.info("Стартуем бота")
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+def escape_markdown_v2(text: str) -> str:
+    # Экранируем все специальные символы MarkdownV2 согласно документации
+    return re.sub(r"([_*[\]()~`>#+\-=|{}.!\\])", r"\\\1", text)
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -98,21 +54,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
 async def get_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # async with httpx.AsyncClient() as client:
-    #     response = await client.get("http://fastapi:8000/api/posts")
-    #     posts = response.json()
-    posts = test_response
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get("http://fastapi:8000/api/posts")
+            posts = response.json()
+        except Exception as e:
+            logger.error("Ошибка при получении постов: %s", e, exc_info=True)
+            await update.message.reply_text(
+                "Сервис времено не доступен, попробуйте позже."
+            )
     if not posts:
         await update.message.reply_text("Постов пока нет.")
         return
@@ -120,7 +71,7 @@ async def get_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard_buttons = [
         [
             InlineKeyboardButton(
-                text=f"{post['title']} - {post['date_of_creation'][:10]}",
+                text=f"{post['title']}",
                 callback_data="post_id:" + str(post["id"]),
             )
         ]
@@ -134,22 +85,60 @@ async def get_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def detail_post_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
-    # post_id = int(query.data.replace("post_id:", ""))
-    if not test_response:
+    post_id = int(query.data.replace("post_id:", ""))
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"http://fastapi:8000/api/posts/{post_id}")
+            post_data = response.json()
+        except Exception as e:
+            logger.error("Ошибка при получении постов: %s", e, exc_info=True)
+
+    if not post_data:
         await query.answer(show_alert=True, text="Данный пост больше не существует")
     await query.answer()
-    text = f"{test_response[1]['title']}\n{test_response[1]['date_of_creation']}\n\n{test_response[1]['text']}"
+
+    text = f"*{post_data['title']}*\nДата создания: _{escape_markdown_v2(post_data['date_of_creation'][:10])}_\n\n{post_data['text']}"
     await query.edit_message_text(
         text=text,
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="Назад", callback_data="posts_menu")]]
         ),
+        parse_mode="MarkdownV2",
+    )
+
+
+async def back_to_post_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get("http://fastapi:8000/api/posts")
+            posts = response.json()
+        except Exception as e:
+            logger.error("Ошибка при получении постов: %s", e, exc_info=True)
+            await update.message.reply_text(
+                "Сервис времено не доступен, попробуйте позже."
+            )
+
+    if not posts:
+        await query.edit_message_text("Постов пока нет.")
+
+    keyboard_buttons = [
+        [
+            InlineKeyboardButton(
+                text=post["title"],
+                callback_data="post_id:" + str(post["id"]),
+            )
+        ]
+        for post in posts
+    ]
+
+    await query.edit_message_text(
+        text="Доступные посты", reply_markup=InlineKeyboardMarkup(keyboard_buttons)
     )
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+
     application = Application.builder().token(token).build()
 
     # on different commands - answer in Telegram
@@ -158,11 +147,10 @@ def main() -> None:
     application.add_handler(
         CallbackQueryHandler(detail_post_info, pattern=r"^post_id:\d+$")
     )
+    application.add_handler(
+        CallbackQueryHandler(back_to_post_menu, pattern=r"^posts_menu")
+    )
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
